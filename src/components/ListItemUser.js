b534db816d93bae1fbe7f0524565d64c9672385d
NathanaelZorens <zorens32@yahoo.com>
2022-11-13 21:07:45 +0700

    Update ListItemUser.js

--- src/components/ListItemUser.js
+++ src/components/ListItemUser.js
@@ -1,1 +1,20 @@
-
+import {
+    ListItem,
+    ListItemText,
+    Typography
+  } from "@mui/material";
+  import React from "react";
+  
+  function ListItemUser({ color, primaryText, secondaryText }) {
+    return (
+      <ListItem >
+        <ListItemText 
+          primary={<Typography color={color} variant="h6">{primaryText}</Typography>}
+          secondary={<Typography variant="p">year: {secondaryText}</Typography>}
+        />
+      </ListItem>
+    );
+  }
+  
+  export default ListItemUser;
+  
